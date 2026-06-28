cc9d4901bb2d4bc54da7851ce1b665bbff80176e
xasanbayevafzalbek-droid <xasanbayevafzalbek@gmail.com>
2026-06-28 08:01:20 +0500

    Add AI task: Kalkulyator dasturi

---
+++ ai_topshiriqlar/Kalkulyator_dasturi.py
@@ -0,0 +1,18 @@
+# Kalkulyator dasturi
+# Kurs: Dasturlash / IT
+# Mavzu: 🛠 Mini-loyiha: Kalkulyator — ikki son ustida amallar (CLI)
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+son1, son2, amal = input().split()
+son1 = int(son1)
+son2 = int(son2)
+if amal == '+':
+    print(son1 + son2)
+elif amal == '-':
+    print(son1 - son2)
+elif amal == '*':
+    print(son1 * son2)
+elif amal == '/':    
+    print(son1 / son2)
