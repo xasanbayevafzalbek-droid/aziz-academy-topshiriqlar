f4674f6e2e06ce682907826accd61a469a1a0340
xasanbayevafzalbek-droid <xasanbayevafzalbek@gmail.com>
2026-07-03 17:23:23 +0500

    Add: Profil kartasi [xasanbayevafzalbek-droid]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,11 @@
+ism = "Aziz"
+familiya = "Karimov" 
+kurs = "Python"
+daraja = "Boshlang'ich"
+shahar = "Toshkent"
+print("=== PROFIL ===")
+print(f"Ism: {ism}")
+print(f"Familiya: {familiya}")
+print(f"Kurs: {kurs}")
+print(f"Daraja: {daraja}")
+print(f"Shahar: {shahar}")
