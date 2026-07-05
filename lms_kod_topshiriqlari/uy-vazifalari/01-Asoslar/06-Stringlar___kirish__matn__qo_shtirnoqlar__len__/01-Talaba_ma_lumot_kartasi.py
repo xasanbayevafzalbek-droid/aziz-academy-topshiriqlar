770a230df5b89a50ea885a0550d5195834ea1178
xasanbayevafzalbek-droid <xasanbayevafzalbek@gmail.com>
2026-07-05 15:21:33 +0500

    Add: Talaba ma'lumot kartasi [xasanbayevafzalbek-droid]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Talaba_ma_lumot_kartasi.py
@@ -0,0 +1,8 @@
+ism = "Aziz Karimov"
+kurs = "Python"
+shahar = "Toshkent"
+print("=== KARTA ===")
+print("Ism:", ism)
+print("Kurs:", kurs)
+print("Shahar:", shahar)
+print("Ism uzunligi:", len(ism))
