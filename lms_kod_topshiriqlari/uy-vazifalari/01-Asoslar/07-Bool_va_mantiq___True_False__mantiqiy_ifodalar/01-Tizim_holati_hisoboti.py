dddec47469ef956415974be7b5b3d214a90e9a37
xasanbayevafzalbek-droid <xasanbayevafzalbek@gmail.com>
2026-07-05 15:29:39 +0500

    Add: Tizim holati hisoboti [xasanbayevafzalbek-droid]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Tizim_holati_hisoboti.py
@@ -0,0 +1,9 @@
+server_ishlayapti = True
+baza_ulangan = True
+xatolik_bor = False
+xavfsiz = True
+print("=== TIZIM HOLATI ===")
+print("Server ishlayapti:", True)
+print("Baza ulangan:", True)
+print("Xatolik bor:", False)
+print("Xavfsiz:", True)
