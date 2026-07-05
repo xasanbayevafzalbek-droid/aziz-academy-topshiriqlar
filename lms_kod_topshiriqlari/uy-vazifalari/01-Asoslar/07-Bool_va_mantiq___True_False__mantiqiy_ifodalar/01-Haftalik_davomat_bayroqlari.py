ff7ef24683712d0a739acda970a2868832278999
xasanbayevafzalbek-droid <xasanbayevafzalbek@gmail.com>
2026-07-05 15:35:15 +0500

    Add: Haftalik davomat bayroqlari [xasanbayevafzalbek-droid]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Haftalik_davomat_bayroqlari.py
@@ -0,0 +1,10 @@
+dushanba = True
+seshanba = True
+chorshanba = False
+payshanba = True
+juma = False
+print("Dushanba:", True)
+print("Seshanba:", True)
+print("Chorshanba:", False)
+print("Payshanba:", True)
+print("Juma:", False)
