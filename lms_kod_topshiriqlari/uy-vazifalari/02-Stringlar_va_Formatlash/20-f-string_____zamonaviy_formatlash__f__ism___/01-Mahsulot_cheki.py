30c5e9508ae12e536eb4c03508ae202a730ce1fa
xasanbayevafzalbek-droid <xasanbayevafzalbek@gmail.com>
2026-07-08 16:54:10 +0500

    Add: Mahsulot cheki [xasanbayevafzalbek-droid]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Mahsulot_cheki.py
@@ -0,0 +1,10 @@
+mahsulot = input()
+narxi = int(input())
+soni = int(input())
+
+jami = narxi * soni
+
+print(f"Mahsulot: {mahsulot}")
+print(f"Narxi: {narxi} som")
+print(f"Soni: {soni}")
+print(f"Jami: {jami} som")
